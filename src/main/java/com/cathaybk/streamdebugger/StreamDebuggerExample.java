8b6fd919eaa3750ad4de6e192bd70ea5bc5cee0f
Roger Lo <sobadrush@icloud.com>
2022-05-08 17:35:45 +0800

    測試intelliJ stream debugger

---
+++ src/main/java/com/cathaybk/streamdebugger/StreamDebuggerExample.java
@@ -0,0 +1,35 @@
+package com.cathaybk.streamdebugger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * @author RogerLo
+ * @date 2022/5/8
+ */
+public class StreamDebuggerExample {
+
+    // 計算value的因數
+    public static IntStream factorize(int value) {
+        List<Integer> facList = new ArrayList<>();
+        for (int i = 2; i <= value; i++) {
+            while (value % i == 0) {
+                facList.add(value);
+                value /= i;
+            }
+        }
+        return facList.stream().mapToInt(val -> Integer.valueOf(val));
+    }
+
+    public static void main(String[] args) {
+        int[] results = IntStream.of(10, 87, 97, 43, 121, 20)
+                .flatMap(StreamDebuggerExample::factorize)
+                .distinct()
+                .sorted()
+                .toArray();
+        System.out.println("results = " + Arrays.toString(results));
+    }
+
+}
