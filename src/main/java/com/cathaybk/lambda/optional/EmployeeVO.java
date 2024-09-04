1727490489c4d309973358200f5c7b6b2cf222d5
Roger Lo <sobadrush@icloud.com>
2024-09-04 21:56:05 +0800

    測試 Optional 的 orElseThrow

--- src/main/java/com/cathaybk/lambda/optional/EmployeeVO.java
+++ src/main/java/com/cathaybk/lambda/optional/EmployeeVO.java
@@ -1,6 +1,12 @@
 package com.cathaybk.lambda.optional;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.Optional;
+import java.util.function.Supplier;
 
 class Room {
 	String roomNum;
@@ -24,9 +30,14 @@ class Floor {
 	}
 }
 
+@ToString
 class Company {
 	String companyName;
 	Floor floor;
+
+	public Company(String companyName) {
+		this.companyName = companyName;
+	}
 
 	public Company(String compName, Floor floor) {
 		this.floor = floor;
@@ -34,33 +45,47 @@ class Company {
 	}
 }
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeVO {
-	Company company;
+	String empName;
+    Company company;
+
+	public EmployeeVO(String empName) {
+		this.empName = empName;
+	}
 
 	public EmployeeVO(Company company) {
 		this.company = company;
 	}
 
-	public Company getCompany() {
-		return company;
-	}
-
-	public static void main(String[] args) {
-//		Floor floor = new Floor(3, new Room("18"));
-		Floor floor = new Floor(new Room("B區_18房"));
-		Company company = new Company("台積電", floor);
-		EmployeeVO emp = new EmployeeVO(company);
-
-//		.map(ss::substring(3));
-		System.out.println(
-				Optional.ofNullable(emp)
-				.map(ee -> ee.getCompany())
-				.map(cc -> cc.floor)
-				.map(ff -> ff.room)
-				.map(rr -> rr.roomNum).map(str -> str.split("_")[1])
-				.map((str) -> {
-					return str + "_GGG_";
-				})
+	public static void main(String[] args) throws Throwable {
+		EmployeeVO empVO = new EmployeeVO("Roger");
+		System.err.println(
+			Optional.ofNullable(empVO)
+				.filter(xx -> !xx.getEmpName().equals("Roger"))
+				.map(x -> new Company(x.getEmpName()))
+				.orElseThrow((Supplier<Throwable>) () -> new RuntimeException("Can't find EmpVO whose name is Roger"))
 		);
 	}
+
+    // public static void main(String[] args) {
+	// 	// Floor floor = new Floor(3, new Room("18"));
+	// 	Floor floor = new Floor(new Room("B區_18房"));
+	// 	Company company = new Company("台積電", floor);
+	// 	EmployeeVO emp = new EmployeeVO(company);
+	//
+	// 	// .map(ss::substring(3));
+	// 	System.out.println(
+	// 			Optional.ofNullable(emp)
+	// 			.map(ee -> ee.getCompany())
+	// 			.map(cc -> cc.floor)
+	// 			.map(ff -> ff.room)
+	// 			.map(rr -> rr.roomNum).map(str -> str.split("_")[1])
+	// 			.map((str) -> {
+	// 				return str + "_GGG_";
+	// 			}).get()
+	// 	);
+	// }
 }
